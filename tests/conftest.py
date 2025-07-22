40e7850ceed4d84390dbaea09d183bd9ac4f8a1e
sviridov_av2 <sviridov_av2@magnit.ru>
2025-07-22 23:55:47 +0300

    update test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,16 @@
 import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from selene import browser
 
-@pytest.fixture(scope = 'function' , autouse=True)
-def browser_open():
-    browser.open('https://demoqa.com/automation-practice-form')
+@pytest.fixture(scope='function', autouse=True)
+def browser_setup():
+    browser.config.driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()))
+    browser.config.base_url = 'https://demoqa.com'
+    browser.config.timeout = 10
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
     yield
     browser.quit()
--- tests/test_form.py
+++ tests/test_form.py
@@ -1,51 +1,27 @@
 from selene import browser, be, have
 import os
 
-
 def test_submit_practice_form():
-    # Открываем страницу с формой
+    file_path = os.path.join(os.path.dirname(__file__), 'testfile.txt')
     browser.open('/automation-practice-form')
-
-    # Заполняем основные поля
-    browser.element('#firstName').should(be.blank).type('Иван')
+    browser.element('#firstName').should(be.blank).type('Петр')
     browser.element('#lastName').should(be.blank).type('Петров')
-    browser.element('#userEmail').should(be.blank).type('ivan.petrov@example.com')
-
-    # Выбираем пол (Male)
+    browser.element('#userEmail').should(be.blank).type('petr.petrov@example.com')
     browser.all('[name=gender]').element_by(have.value('Male')).element('..').click()
-
-    # Вводим номер телефона
     browser.element('#userNumber').should(be.blank).type('1234567890')
-
-    # Выбираем дату рождения (15 May 1990)
     browser.element('#dateOfBirthInput').click()
     browser.element('.react-datepicker__month-select').type('May')
-    browser.element('.react-datepicker__year-select').type('1990')
-    browser.element('.react-datepicker__day--015').click()
-
-    # Выбираем предмет (Computer Science)
+    browser.element('.react-datepicker__year-select').type('1985')
+    browser.element('.react-datepicker__day--010').click()
     browser.element('#subjectsInput').type('Computer Science').press_enter()
-
-    # Выбираем хобби (Sports)
-    browser.all('.custom-checkbox').element_by(have.exact_text('Sports')).click()
-
-    # Загружаем файл (предварительно создайте файл test.txt в папке с тестом)
-    file_path = os.path.join(os.path.dirname(__file__), 'test.txt')
-    browser.element('#uploadPicture').set_value(file_path)
-
-    # Вводим адрес
-    browser.element('#currentAddress').should(be.blank).type('ул. Примерная, 123')
-
-    # Выбираем штат и город
+    browser.all('.custom-checkbox').element_by(have.exact_text('Waha')).click()
+    browser.element('#uploadPicture').send_keys(file_path)
+    browser.element('#currentAddress').type('ул. Тестовая, 007')
     browser.element('#state').click()
-    browser.element('#react-select-3-option-0').click()  # NCR
+    browser.element('#react-select-3-option-0').click()
     browser.element('#city').click()
-    browser.element('#react-select-4-option-0').click()  # Delhi
-
-    # Отправляем форму
+    browser.element('#react-select-4-option-0').click()
     browser.element('#submit').press_enter()
-
-    # Проверяем результаты
     browser.element('.modal-header').should(have.text('Thanks for submitting the form'))
     browser.all('.table-responsive td:nth-child(2)').should(have.texts(
         'Иван Петров',
@@ -55,7 +31,7 @@ def test_submit_practice_form():
         '15 May,1990',
         'Computer Science',
         'Sports',
-        'test.txt',
+        'testfile.txt',
         'ул. Примерная, 123',
         'NCR Delhi'
     ))
